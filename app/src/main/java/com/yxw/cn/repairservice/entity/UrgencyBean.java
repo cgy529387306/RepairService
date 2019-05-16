4a9110bcf4f23d8eda84500d8d40034e0ca5cea1
THINK <529387306@qq.com>
2019-05-16 22:34:26 +0800

    --------------------------

--- app/src/main/java/com/yxw/cn/repairservice/entity/UrgencyBean.java
+++ app/src/main/java/com/yxw/cn/repairservice/entity/UrgencyBean.java
@@ -21,39 +21,14 @@ public class UrgencyBean implements Serializable{
      * value : null
      */
 
-    private Object createId;
-    private Object createTime;
     private String dictId;
     private String mark;
     private String markName;
     private String name;
-    private Object open;
-    private Object parentId;
     private String remark;
-    private Object sort;
-    private Object type;
-    private Object updateId;
-    private Object updateTime;
-    private Object value;
-
-    public Object getCreateId() {
-        return createId;
-    }
-
-    public void setCreateId(Object createId) {
-        this.createId = createId;
-    }
-
-    public Object getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Object createTime) {
-        this.createTime = createTime;
-    }
 
     public String getDictId() {
-        return dictId;
+        return dictId == null ? "" : dictId;
     }
 
     public void setDictId(String dictId) {
@@ -61,7 +36,7 @@ public class UrgencyBean implements Serializable{
     }
 
     public String getMark() {
-        return mark;
+        return mark == null ? "" : mark;
     }
 
     public void setMark(String mark) {
@@ -69,7 +44,7 @@ public class UrgencyBean implements Serializable{
     }
 
     public String getMarkName() {
-        return markName;
+        return markName == null ? "" : markName;
     }
 
     public void setMarkName(String markName) {
@@ -77,74 +52,18 @@ public class UrgencyBean implements Serializable{
     }
 
     public String getName() {
-        return name;
+        return name == null ? "" : name;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public Object getOpen() {
-        return open;
-    }
-
-    public void setOpen(Object open) {
-        this.open = open;
-    }
-
-    public Object getParentId() {
-        return parentId;
-    }
-
-    public void setParentId(Object parentId) {
-        this.parentId = parentId;
-    }
-
     public String getRemark() {
-        return remark;
+        return remark == null ? "" : remark;
     }
 
     public void setRemark(String remark) {
         this.remark = remark;
-    }
-
-    public Object getSort() {
-        return sort;
-    }
-
-    public void setSort(Object sort) {
-        this.sort = sort;
-    }
-
-    public Object getType() {
-        return type;
-    }
-
-    public void setType(Object type) {
-        this.type = type;
-    }
-
-    public Object getUpdateId() {
-        return updateId;
-    }
-
-    public void setUpdateId(Object updateId) {
-        this.updateId = updateId;
-    }
-
-    public Object getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Object updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    public Object getValue() {
-        return value;
-    }
-
-    public void setValue(Object value) {
-        this.value = value;
     }
 }
